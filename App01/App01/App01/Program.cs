177bb0b68e23dfe25dc31ace2798fb24b8ea23ff
fernando98-mc <luisf543678@gmail.com>
2023-07-19 13:28:48 -0600

    type data

--- App01/App01/App01/Program.cs
+++ App01/App01/App01/Program.cs
@@ -8,4 +8,25 @@
 string nombre = "Dayu";
 var pagina = "http://WWW.mipagina.com";
 
+int miNumeroEntero = 100;
+int otroNumeroEntero = -50;
+
+float miFloatNumero = 34.65f;
+
+double miDoubleNumero = 132.21;
+
+// transacciones money
+decimal miDecimal = 123.32423m;
+
+//max 250 en byte
+byte miNumeroByte = 249;
+
+bool miBooleano = true;
+
+string miCadena = "es una cadena de texto";
+
+// solo alamacena un caracter
+char micaracter = 'A';
+
+
 Console.WriteLine("Hello, soy " + nombre + " y mi pagina es " + pagina);
