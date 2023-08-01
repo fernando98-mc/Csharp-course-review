cd42c288160fda78894f275493030b0d74b64b9f
fernando98-mc <luisf543678@gmail.com>
2023-08-01 12:56:03 -0600

    bucle for

--- App01/App01/Bucles/Program.cs
+++ App01/App01/Bucles/Program.cs
@@ -2,7 +2,25 @@
 
 // Imprimi el texto 80 veces  : Bienvnido al curso
 
+/*
 for(int i = 1; i <=80; i++)
 {
     Console.WriteLine($"{i} - Bienvenido : ) ");
+}
+*/
+
+
+// 1 - El usuario ingresa un numero en la consola
+// 2 - El programa se multiplica por los primeros 10 numero ingresados
+// 3 - Se debe imprimir el resultado de cada operacion
+
+Console.WriteLine("Ingresa un numero para multiplicar");
+var numeroMulyiplicado = int.Parse(Console.ReadLine());
+
+Console.WriteLine("------------");
+
+for (int i = 1; i <= 10; i++)
+{
+    int resultado = numeroMulyiplicado * i;
+    Console.WriteLine($"{numeroMulyiplicado} x {i} = {resultado}");
 }
