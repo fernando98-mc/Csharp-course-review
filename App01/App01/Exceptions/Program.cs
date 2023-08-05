7261eaa87d06c907f3f50bd1f85ffeae27279563
fernando98-mc <luisf543678@gmail.com>
2023-08-04 20:47:29 -0600

    add loop with exception simple FormatException

---
+++ App01/App01/Exceptions/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+// 1 - El usuarioingresa n veces numeros a sumarse
+// 2 - Si el usuario ingres un numero = 0 entonces debe detener el programa
+// 3 - Imprime el total de la suma, de los numero ingresados
+
+
+int acumulador = 0;
+int numero = 0;
+
+do
+{
+    Console.WriteLine("Ingresa los numero a sumar");
+
+    try
+    {
+        numero = int.Parse(Console.ReadLine()!);
+    }
+    /* catch (Exception e) */ // Forma generica si no sabemos el error
+    catch(System.FormatException e)
+    {
+        /* Imprimimos el error */
+        Console.WriteLine($"El tipo de error fue por : {e}");
+        
+    }
+
+    acumulador += numero;
+
+} while (numero != 0);
+
+Console.WriteLine($"El total de la suma es de : {acumulador}");
