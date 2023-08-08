fb135ea0e584636b2e905351f2e607c76adbd04b
fernando98-mc <luisf543678@gmail.com>
2023-08-08 14:32:14 -0600

    add method number random

--- App01/App01/method-void/Program.cs
+++ App01/App01/method-void/Program.cs
@@ -1,9 +1,36 @@
 ﻿
 
 
-void ImprimirTexto()
+//void ImprimirTexto()
+//{
+//    Console.WriteLine("Hola soy un metodo");
+//}
+
+//ImprimirTexto();
+
+
+// 1 - El usuario ingresa el numero que representa cauntas veces se generara 
+//      este valor aleratorio
+// 2 - En el bucle se generara un numero aleatorio entre 1 y 1000
+// 3 - Imprimir dentro del bucle este numero aleatorio previamente generado
+
+
+void ImprimirNumero()
 {
-    Console.WriteLine("Hola soy un metodo");
+
+    Console.WriteLine("Ingreaa un numero alerario");
+
+    var numeroRandom = int.Parse(Console.ReadLine());
+
+    var random = new Random();
+
+    for (int i =1; i <= numeroRandom; i++)
+    {
+        var numeroAleatorio = random.Next(1 , 100);
+
+        Console.WriteLine($"EL numero random es  : {numeroAleatorio}");
+    }
+
 }
 
-ImprimirTexto();
+ImprimirNumero();
