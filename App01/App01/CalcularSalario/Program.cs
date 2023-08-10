d62c3380b303e0643a546d7547849aed15fc9e5a
fernando98-mc <luisf543678@gmail.com>
2023-08-10 13:56:10 -0600

    method strind return

---
+++ App01/App01/CalcularSalario/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+// Objetivo : Calcular el sueldo total de un empleado
+// 1 - El metodo recibe los valores de salario, impuesto  y salud
+// 2 - calcula el total que debe recibir de salario en funcion a estos parametros
+// 3 - El metodo debe devolver el salario total
+
+
+
+// PARAMETROS POR REFERENCIA
+
+
+                                                        // parametro por referecia,  salida " out "
+void CalcularSalario(int salario, int impuesto, int salud, out int total)
+{
+    total = salario - impuesto - salud;
+}
+
+var salarioTotal = 0;
+                                        //  recipiente de "total"
+CalcularSalario(salario:300, impuesto:50, salud:50, out salarioTotal);
+
+Console.WriteLine($"El salario total fue de : {salarioTotal}");
---
+++ App01/App01/method-string/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+// Verificar si es mayor de edad
+
+
+string GetMensajeEdad(int edad)
+{
+
+    if (edad > 18)
+    {
+        return "Es mayor de edad";
+    } else
+    {
+        return "Es menor de edad";
+    }
+
+}
+
+var mensaje = GetMensajeEdad(17);
+
+Console.WriteLine($"La persona es : {mensaje}");
