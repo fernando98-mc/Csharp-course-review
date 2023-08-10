486a488c188ae81fd417f111dd4fa91f5a4a0897
fernando98-mc <luisf543678@gmail.com>
2023-08-10 14:34:50 -0600

    tupla

---
+++ App01/App01/tuplas/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+(int a, string b, bool c) tupla = (15, "Daya", true);
+
+var tuplaA = tupla.a;
+var tuplaB = tupla.b;
+var tuplaC = tupla.c;
+
+Console.WriteLine($"La edad es de : {tuplaA}");
+Console.WriteLine($"El nombre es  : {tuplaB}");
+Console.WriteLine($"Es chica : {tuplaC}");
