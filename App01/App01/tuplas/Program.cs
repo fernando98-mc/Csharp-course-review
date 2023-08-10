53184c673d208d453287cbfcb74c6d1dac872762
fernando98-mc <luisf543678@gmail.com>
2023-08-10 14:46:44 -0600

    set data tupla

--- App01/App01/tuplas/Program.cs
+++ App01/App01/tuplas/Program.cs
@@ -2,9 +2,16 @@
 
 
 (int a, string b, bool c) tupla = (15, "Daya", true);
+//var tupla = (15, "Daya", true);   // forma IMPLICITA
 
+// setear datos desde la ejecucion
+tupla.a = 17;
 var tuplaA = tupla.a;
+
+tupla.b = "Dayaa";
 var tuplaB = tupla.b;
+
+tupla.c = false;
 var tuplaC = tupla.c;
 
 Console.WriteLine($"La edad es de : {tuplaA}");
