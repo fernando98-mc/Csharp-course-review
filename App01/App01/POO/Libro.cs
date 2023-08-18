b1283536868496a4292ad6c15d519e385ac0e548
fernando98-mc <luisf543678@gmail.com>
2023-08-17 19:48:15 -0600

    add class libro

---
+++ App01/App01/POO/Libro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Libro
+    {
+        // propiedades
+        string _nombre;
+        string _autor;
+        int _cantidadPaginas;
+
+        // constructor          parametros
+        public Libro(string nombre, string autor, int cantidadPaginas)
+        {
+            _nombre = nombre;
+            _autor = autor;
+            _cantidadPaginas = cantidadPaginas;
+        }
+
+    }
+}
