01b28210b3bf8456cea882629fe75a8e3ca0c05b
fernando98-mc <luisf543678@gmail.com>
2023-08-18 20:21:36 -0600

    add calls methos access

--- App01/App01/POO/Program.cs
+++ App01/App01/POO/Program.cs
@@ -4,10 +4,16 @@ using POO;
 // nombre de clase nombre del objeto instanciar y argumentos
 Libro donQuijote = new Libro("Don quijote", "Miguel de cervantes", 100);
 
-donQuijote._nombre = "Don quijote 2";
+//donQuijote._nombre = "Don quijote 2";
 
 donQuijote.SetNombre("Don quijote 3");
 var nuevoNombre = donQuijote.GetNombre();
+
+donQuijote.SetAutor("Cervantes");
+var nuevoAutor = donQuijote.GetAutor();
+
+donQuijote.SetNumeroPaginas(1000);
+var nuevaCantidad = donQuijote.GetNumeroPaginas();
 
 var resultado = donQuijote.GetDescripcionLibro();
 
