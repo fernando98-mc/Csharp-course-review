a9d0cf6ed82e4dd48b24b4241a3bbce8595439a3
fernando98-mc <luisf543678@gmail.com>
2023-08-18 19:40:39 -0600

    add method access att nombre

--- App01/App01/POO/Program.cs
+++ App01/App01/POO/Program.cs
@@ -6,6 +6,9 @@ Libro donQuijote = new Libro("Don quijote", "Miguel de cervantes", 100);
 
 donQuijote._nombre = "Don quijote 2";
 
+donQuijote.SetNombre("Don quijote 3");
+var nuevoNombre = donQuijote.GetNombre();
+
 var resultado = donQuijote.GetDescripcionLibro();
 
 Console.WriteLine(resultado);
