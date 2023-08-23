52441159dc5bbf0c9d7b04c17cd053e25d36fac4
fernando98-mc <luisf543678@gmail.com>
2023-08-22 20:48:25 -0600

    order and deletem items

--- App02/Appp02/Appp02/Program.cs
+++ App02/Appp02/Appp02/Program.cs
@@ -2,8 +2,8 @@
 
 
 // array de personas tipo string
-string[] personas = { "Daya", "Mani", "Cele" };
-//  posicion            0       1       2
+string[] personas = { "Daya", "Mani", "Cele", "Gise", "Gin" };
+//  posicion            0       1       2       3       4
 
 
 // List Collection - creacion de objeto lista
@@ -20,6 +20,13 @@ Console.WriteLine($"Hay un total de {listaPersonas.Count} chicas :)");
 //Console.WriteLine(listaPersonas[2]);
 
 
+// ordenar alfabeticamente la lista
+listaPersonas.Sort();
+
+// eliminar un elemento de la lista
+listaPersonas.RemoveAt(3);     // por indice
+listaPersonas.Remove("Mani");  // por valor
+
 
 // crear un metodo para imprimir todos los elementos de la lista
 void ImprimirPersonas(List<string> imprimirPersonas)
@@ -31,3 +38,6 @@ void ImprimirPersonas(List<string> imprimirPersonas)
 }
 
 ImprimirPersonas(listaPersonas);
+
+// consultar un elemento de la lista
+Console.WriteLine($"El primer elemento de la lista es - {listaPersonas[0]}");
