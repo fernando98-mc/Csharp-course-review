c3f5d26d1a46e11a02465856d6bf88c152d43643
fernando98-mc <luisf543678@gmail.com>
2023-08-22 20:19:27 -0600

    add List personas

---
+++ App02/Appp02/Appp02/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+// array de personas tipo string
+string[] personas = { "Daya", "Mani", "Cele" };
+//  posicion            0       1       2
+
+
+// List Collection - creacion de objeto lista
+List<string> listaPersonas = new List<string>();
+
+// agregar data a la lista desde un array
+listaPersonas.AddRange(personas);
+
+// agregar un elemento a la lista
+listaPersonas.Add("Sera");
+
+// contar cuantas personas hay en la lista
+Console.WriteLine($"Hay un total de {listaPersonas.Count} chicas :)");
+Console.WriteLine(listaPersonas[2]);
