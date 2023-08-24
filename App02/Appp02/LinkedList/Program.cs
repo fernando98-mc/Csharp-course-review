8fc8439337d8312f4e876efc333ec9cc7a33db71
fernando98-mc <luisf543678@gmail.com>
2023-08-24 14:25:38 -0600

    add LinkedList

---
+++ App02/Appp02/LinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+string[] ropa= { 
+    "Tennis",
+    "Sandalias",
+    "Playera",
+    "Camisa",
+    "Blusa",
+    "Pantalon",
+    "Ropa interior",
+    "Short"
+};
+
+
+// Creando LinkedList                        cargar datos "ropa"
+LinkedList<string> ropaLinkedList = new LinkedList<string>(ropa);
+
+// Agregamos elementos a la lista inicio
+ropaLinkedList.AddFirst("Corbata");
+
+// Agregamos elementos a la lista final
+ropaLinkedList.AddLast("Pants");
+
+foreach (string listaRopa in ropaLinkedList)
+{
+    Console.WriteLine(listaRopa);
+}
