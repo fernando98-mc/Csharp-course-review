4ff18c800a68ddb385a3916ab6fb20278b9b7bf8
fernando98-mc <luisf543678@gmail.com>
2023-08-25 13:01:13 -0600

    add Stack

---
+++ App02/Appp02/Stack/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+// crear un stack collection
+Stack<string> stackNumeros = new Stack<string>();
+
+// Agregar datos al stack
+stackNumeros.Push("uno");
+stackNumeros.Push("dos");
+stackNumeros.Push("tres");    
+stackNumeros.Push("cuatro");
+stackNumeros.Push("cinco");
+
+// imprimir numeros del stack
+foreach (string impNumeros in stackNumeros)
+{
+    Console.WriteLine(impNumeros);
+}
+
