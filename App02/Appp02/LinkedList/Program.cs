bfa7fc49aba6d466bd556aed5980e457de59df31
fernando98-mc <luisf543678@gmail.com>
2023-08-24 19:43:06 -0600

    add method addAfter()

--- App02/Appp02/LinkedList/Program.cs
+++ App02/Appp02/LinkedList/Program.cs
@@ -38,3 +38,13 @@ LinkedListNode<string> ultimaRopa = ropaLinkedList.Last!;
 //                              "value" imprime el valor string 
 Console.WriteLine($"La primera ropa es : {primerRopa.Value}");
 Console.WriteLine($"La ultima ropa es : {ultimaRopa.Value}");
+
+// Los elemento pueden ser removidos o eliminados de un item existente
+ropaLinkedList.AddAfter(primerRopa, "Traje");
+
+foreach (string listaRopa in ropaLinkedList)
+{
+    Console.WriteLine(listaRopa);
+}
+
+Console.WriteLine($"Se agrego traje en la lista ");
