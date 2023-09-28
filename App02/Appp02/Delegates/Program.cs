aa006310ccaf5212edf1462682fa4ae5473a39f6
fernando98-mc <luisf543678@gmail.com>
2023-09-28 13:58:30 -0600

    add code use deledate class MiFunction -> 'objeto2'

--- App02/Appp02/Delegates/Program.cs
+++ App02/Appp02/Delegates/Program.cs
@@ -14,7 +14,9 @@ Console.WriteLine($"La suma es : {f(5,9)}");
 f = objeto.functMult;
 Console.WriteLine($"La multiplicacion es : {f(343,34)}");
 
-
+var objeto2 = new MiFunction();
+f = objeto2.IntanciarDeMetodo;
+Console.WriteLine($"El resultado de la operacion es : {f(32234,324)}");
 
 public delegate string MiDelegate(int arg1, int arg2);
 
