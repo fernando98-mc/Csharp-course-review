f796126420c684f138fd55acc8eee86e3a2924ef
fernando98-mc <luisf543678@gmail.com>
2023-09-28 13:43:20 -0600

    add code suma use delegate

--- App02/Appp02/Delegates/Program.cs
+++ App02/Appp02/Delegates/Program.cs
@@ -3,6 +3,15 @@
 /**
  *  crear delegate
  */
+using Delegates;
+
+var objeto = new classFunctions();
+
+MiDelegate f = objeto.funtSuma;
+
+Console.WriteLine($"La suma es : {f(5,9)}");
 
 
-public delegate string MiDelegate(int arg1, int arg2); 
+
+public delegate string MiDelegate(int arg1, int arg2);
+
