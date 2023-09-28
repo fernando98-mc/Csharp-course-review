9bec12c77b74b282bbd7d0d7c035cb32fea98dc1
fernando98-mc <luisf543678@gmail.com>
2023-09-28 13:43:53 -0600

    add code multi use deledate

--- App02/Appp02/Delegates/Program.cs
+++ App02/Appp02/Delegates/Program.cs
@@ -11,7 +11,8 @@ MiDelegate f = objeto.funtSuma;
 
 Console.WriteLine($"La suma es : {f(5,9)}");
 
-
+f = objeto.functMult;
+Console.WriteLine($"La multiplicacion es : {f(343,34)}");
 
 public delegate string MiDelegate(int arg1, int arg2);
 
