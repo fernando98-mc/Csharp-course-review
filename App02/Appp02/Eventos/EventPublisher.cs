b5bf5c9747d9e3767c1e42bbc5820ea84c5b2a67
fernando98-mc <luisf543678@gmail.com>
2023-10-11 14:34:29 -0600

    applicated event

---
+++ App02/Appp02/Eventos/EventPublisher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventos
+{
+    public class EventPublisher
+    {
+
+        public String TheVal;
+
+        public event MiEventoHandler valueChanged;
+        public String Val
+        {
+            set
+            {
+                this.TheVal = value;
+                this.valueChanged(value);
+            }
+        }
+
+    }
+}
--- App02/Appp02/Eventos/Program.cs
+++ App02/Appp02/Eventos/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Eventos;
+
+
+var obj = new EventPublisher();
+
+obj.valueChanged += new MiEventoHandler(obj_valueChanged);
+
+void obj_valueChanged(String value)
+{
+    Console.WriteLine($"El valor de la propiedad cambio {value}");
+}
+
+string palabra;
+
+do
+{
+    Console.WriteLine("Ingresa un valor");
+    palabra = Console.ReadLine()!;
+
+    if (!palabra.Equals("exit"))
+    {
+        obj.Val = palabra;
+    }
+
+} while (!palabra.Equals("exit"));
+
+Console.WriteLine("El programa se cerro por el exit");
+
+public delegate void MiEventoHandler(String value);
