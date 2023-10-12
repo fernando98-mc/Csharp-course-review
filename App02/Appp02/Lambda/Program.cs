6912b3a4b0cdea68ab94ac3c395fda110c64f360
fernando98-mc <luisf543678@gmail.com>
2023-10-12 13:28:24 -0600

    add ft1 in two function

--- App02/Appp02/Lambda/Program.cs
+++ App02/Appp02/Lambda/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+MiDelegate ft1 = (x) => x * x;
+Console.WriteLine($"El resultado es : {ft1(6)}");
+
+ft1 = (x) => x * 10;
+Console.WriteLine($"El resultado de multiplicar por 10 : {ft1(5)}");
+
+
+
+public delegate int MiDelegate(int x);
