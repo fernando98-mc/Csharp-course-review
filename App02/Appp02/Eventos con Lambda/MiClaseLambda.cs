0f2c306c9e36fffcf33642358823af5148aa1086
fernando98-mc <luisf543678@gmail.com>
2023-10-12 14:16:51 -0600

    add class 'miclaselambda'

---
+++ App02/Appp02/Eventos con Lambda/MiClaseLambda.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Eventos_con_Lambda
+{
+    public class MiClaseLambda
+    {
+        private string theVal;
+        private event MiEventoHandler valueChange;
+
+        public string val
+        {
+            set
+            {
+                // accionar el evento
+                this.theVal = value;
+                this.valueChange(theVal);
+            }
+        }
+
+    }
+}
