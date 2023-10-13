fcf913284243a7474b297a77b88e48845cd0b0d1
fernando98-mc <luisf543678@gmail.com>
2023-10-13 14:07:12 -0600

    implements methods

--- App03/App03/App03/Documento.cs
+++ App03/App03/App03/Documento.cs
@@ -2,14 +2,29 @@
 
 namespace App03
 {
-    public class Documento
+    public class Documento : IOperaciones
     {
-        public string nombre;
+        private string nombre;
 
         //  cosntructor
         public Documento(string s)
         {
             this.nombre = s;
+        }
+
+        public void Cargar()
+        {
+            Console.WriteLine("Este metodo carga");
+        }
+
+        public void Guardar()
+        {
+            Console.WriteLine("Este metodo guarda");
+        }
+
+        public bool NecesitaGuardar()
+        {
+            return false;
         }
     }
 }
