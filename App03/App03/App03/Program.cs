9d35f2f8860c12d91bf6441bbeb9bb7c91677402
fernando98-mc <luisf543678@gmail.com>
2023-10-13 14:14:23 -0600

    add implementation methos

--- App03/App03/App03/Program.cs
+++ App03/App03/App03/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using App03;
+
+var documento = new Documento("Dayaa");
+documento.Guardar();
+documento.Cargar();
+documento.NecesitaGuardar();
