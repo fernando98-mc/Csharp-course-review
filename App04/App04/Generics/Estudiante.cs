357552a3a86ccd2545ef955dbbb35be3899e5718
fernando98-mc <luisf543678@gmail.com>
2023-10-19 13:54:09 -0600

    add class 'Estudiante'

---
+++ App04/App04/Generics/Estudiante.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Generics
+{
+    public class Estudiante
+    {
+
+        public string? Nombre { set; get; }
+        public string? Apellido { set; get; }
+
+        public Estudiante(string? nombre, string? apellido)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
