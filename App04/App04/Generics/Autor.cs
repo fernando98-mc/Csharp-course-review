d41474ef089c47b6235cb149668d64ff2c195262
fernando98-mc <luisf543678@gmail.com>
2023-10-23 12:49:19 -0600

    add method CompareTo with To String

---
+++ App04/App04/Generics/Autor.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Generics
+{
+    public class Autor : IComparable
+    {
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nombre} {Apellido}";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            
+
+            
+        }
+    }
+}
